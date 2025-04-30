c4c98caf126f5a315dfd2df18e9bd7569ed19081
Sai-guru <lpnkaveri@gmail.com>
2025-04-30 16:08:44 +0530

    hosted backend url

--- frontend/src/sections/Contact/Contact.jsx
+++ frontend/src/sections/Contact/Contact.jsx
@@ -14,7 +14,7 @@ function Contact() {
     };
   
     try {
-      const response = await fetch("http://localhost:5000/send-email", {
+      const response = await fetch("https://portfolio-prigeesh-backend.onrender.com/send-email", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
